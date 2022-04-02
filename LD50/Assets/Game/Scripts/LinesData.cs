ac46e08f17ee02d9f7b816e31e3c1364acdd85f2
mulokun <k.chambonnet@gmail.com>
2022-04-02 15:06:35 +0200

    feat: add LinesData scriptable object

---
+++ LD50/Assets/Game/Scripts/LinesData.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new_line_data", menuName = "Datas/New Lines", order = 0)]
+public class LinesData : ScriptableObject
+{
+    public string[] Lines;
+    public int CurrentLine { get; set; } = 0;
+    public bool IsLast => CurrentLine >= Lines.Length;
+
+    public void Reset()
+    {
+        CurrentLine = 0;
+    }
+
+    public bool TryGetNextLine(out string line)
+    {
+        line = string.Empty;
+
+        bool b = !IsLast;
+        if (b)
+        {
+            line = Lines[CurrentLine];
+            CurrentLine++;
+        }
+        return b;
+    }
+}
